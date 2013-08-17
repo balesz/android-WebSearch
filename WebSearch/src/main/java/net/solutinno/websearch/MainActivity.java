469835f39a145449ddf588683bf5faf1f82952d7
Bode Balint <balint.bode@gmail.com>
2013-08-17 14:55:49 +0200

    Small bug fix

--- WebSearch/src/main/java/net/solutinno/websearch/MainActivity.java
+++ WebSearch/src/main/java/net/solutinno/websearch/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -19,54 +20,63 @@ import net.solutinno.websearch.data.SearchEngineCursor;
 
 import java.util.UUID;
 
-public class MainActivity extends ActionBarActivity {
-
-    Menu mMainMenu;
-    DrawerLayout mDrawerLayout;
-    ListFragment mListFragment;
-    FrameLayout mDetailContainer;
+public class MainActivity extends ActionBarActivity
+{
+    Menu mainMenu;
+    DrawerLayout drawerLayout;
+    ListFragment listFragment;
+    FrameLayout detailContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_list);
+        listFragment.setSelectItemListener(selectItemListener);
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        setupDrawer();
+    }
 
-        mListFragment = (ListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_list);
-        mListFragment.setSelectItemListener(mSelectItemListener);
+    private void setupDrawer() {
+        if (drawerLayout != null) {
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+            drawerLayout.setDrawerListener(drawerListener);
+            detailContainer = (FrameLayout) findViewById(R.id.layout_detail_container);
+            calculateDrawerWidth();
+        }
+    }
 
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
-
-        if (mDrawerLayout != null) {
-            mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
-            mDrawerLayout.setDrawerListener(mDrawerListener);
-
-            mDetailContainer = (FrameLayout) findViewById(R.id.layout_detail_container);
-
-            DisplayMetrics metrics = getResources().getDisplayMetrics();
-            int orientation = getResources().getConfiguration().orientation;
-            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                ViewGroup.LayoutParams layoutParams = mDetailContainer.getLayoutParams();
-                if (layoutParams != null) {
-                    layoutParams.width = (metrics.widthPixels / 3) * 2;
-                    mDetailContainer.setLayoutParams(layoutParams);
-                }
+    private void calculateDrawerWidth() {
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ViewGroup.LayoutParams layoutParams = detailContainer.getLayoutParams();
+            if (layoutParams != null) {
+                layoutParams.width = (metrics.widthPixels / 3) * 2;
+                detailContainer.setLayoutParams(layoutParams);
             }
         }
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+        deleteDetailFragment();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         menu.clear();
-        if (mMainMenu == null) mMainMenu = menu;
+        if (mainMenu == null) mainMenu = menu;
         if (!isDetailFragmentShown()) {
             getSupportActionBar().setIcon(R.drawable.ic_launcher);
             getSupportActionBar().setTitle(R.string.app_name);
-            getMenuInflater().inflate(R.menu.main, mMainMenu);
+            getMenuInflater().inflate(R.menu.main, mainMenu);
         }
         else {
-            getMenuInflater().inflate(R.menu.detail, mMainMenu);
+            getMenuInflater().inflate(R.menu.detail, mainMenu);
         }
-        return super.onCreateOptionsMenu(mMainMenu);
+        return super.onCreateOptionsMenu(mainMenu);
     }
 
     @Override
@@ -74,11 +84,11 @@ public class MainActivity extends ActionBarActivity {
         switch (item.getItemId())
         {
             case R.id.action_add:
-                Add();
+                add();
                 break;
             case R.id.action_import:
                 ImportFragment fragment = ImportFragment.newInstance();
-                fragment.setOnImportDialogResult(mOnImportDialogResult);
+                fragment.setOnImportDialogResult(onImportDialogResult);
                 fragment.show(getSupportFragmentManager(), "import");
                 break;
             default:
@@ -90,8 +100,70 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    private boolean isDetailFragmentShown() {
-        return mDrawerLayout != null && mDrawerLayout.isDrawerOpen(GravityCompat.END);
+    ImportFragment.ImportDialogResult onImportDialogResult = new ImportFragment.ImportDialogResult() {
+        @Override
+        public void OnDialogResult(int result) {
+            if (result == Activity.RESULT_OK) {
+                listFragment.getLoaderManager().getLoader(0).forceLoad();
+            }
+        }
+    };
+
+    ListFragment.SelectItemListener selectItemListener = new ListFragment.SelectItemListener() {
+        @Override
+        public void onSelectItem(UUID id) {
+            if (drawerLayout != null) {
+                createDetailFragment(id);
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+            else {
+                Intent detail = new Intent(getBaseContext(), DetailActivity.class);
+                detail.putExtra(SearchEngineCursor.COLUMN_ID, id.toString());
+                startActivityForResult(detail, 0);
+            }
+        }
+    };
+
+    DrawerLayout.DrawerListener drawerListener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerOpened(View view) {
+            onCreateOptionsMenu(mainMenu);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+        @Override
+        public void onDrawerClosed(View view) {
+            deleteDetailFragment();
+            listFragment.getLoaderManager().getLoader(0).forceLoad();
+            onCreateOptionsMenu(mainMenu);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setHomeButtonEnabled(false);
+        }
+        @Override
+        public void onDrawerSlide(View view, float v) {
+        }
+        @Override
+        public void onDrawerStateChanged(int i) {
+        }
+    };
+
+    DetailFragment.CloseListener detailCloseListener = new DetailFragment.CloseListener() {
+        @Override
+        public void onDetailClosed(int mode, SearchEngine engine) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }
+    };
+
+
+    private void add() {
+        if (drawerLayout != null) {
+            createDetailFragment(null);
+            drawerLayout.openDrawer(GravityCompat.END);
+        }
+        else {
+            Intent detail = new Intent(this, DetailActivity.class);
+            startActivity(detail);
+        }
     }
 
     private DetailFragment getDetailFragment() {
@@ -104,76 +176,20 @@ public class MainActivity extends ActionBarActivity {
         if (id != null) args.putString(SearchEngineCursor.COLUMN_ID, id.toString());
         DetailFragment fragment = new DetailFragment();
         fragment.setArguments(args);
-        fragment.SetDetailCloseListener(mDetailCloseListener);
+        fragment.SetDetailCloseListener(detailCloseListener);
         getSupportFragmentManager().beginTransaction()
             .add(R.id.layout_detail_container, fragment, DetailFragment.class.getName())
             .commit();
     }
 
-    ImportFragment.ImportDialogResult mOnImportDialogResult = new ImportFragment.ImportDialogResult() {
-        @Override
-        public void OnDialogResult(int result) {
-            if (result == Activity.RESULT_OK) {
-                mListFragment.getLoaderManager().getLoader(0).forceLoad();
-            }
+    private void deleteDetailFragment() {
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(DetailFragment.class.getName());
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
         }
-    };
+    }
 
-    ListFragment.SelectItemListener mSelectItemListener = new ListFragment.SelectItemListener() {
-        @Override
-        public void onSelectItem(UUID id) {
-            if (mDrawerLayout != null) {
-                createDetailFragment(id);
-                mDrawerLayout.openDrawer(GravityCompat.END);
-            }
-            else {
-                Intent detail = new Intent(getBaseContext(), DetailActivity.class);
-                detail.putExtra(SearchEngineCursor.COLUMN_ID, id.toString());
-                startActivityForResult(detail, 0);
-            }
-        }
-    };
-
-    DrawerLayout.DrawerListener mDrawerListener = new DrawerLayout.DrawerListener() {
-        @Override
-        public void onDrawerOpened(View view) {
-            onCreateOptionsMenu(mMainMenu);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setHomeButtonEnabled(true);
-        }
-        @Override
-        public void onDrawerClosed(View view) {
-            getSupportFragmentManager().beginTransaction()
-                .remove(getSupportFragmentManager().findFragmentByTag(DetailFragment.class.getName()))
-                .commit();
-            mListFragment.getLoaderManager().getLoader(0).forceLoad();
-            onCreateOptionsMenu(mMainMenu);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportActionBar().setHomeButtonEnabled(false);
-        }
-        @Override
-        public void onDrawerSlide(View view, float v) {
-        }
-        @Override
-        public void onDrawerStateChanged(int i) {
-        }
-    };
-
-    DetailFragment.CloseListener mDetailCloseListener = new DetailFragment.CloseListener() {
-        @Override
-        public void onDetailClosed(int mode, SearchEngine engine) {
-            mDrawerLayout.closeDrawer(GravityCompat.END);
-        }
-    };
-
-    private void Add() {
-        if (mDrawerLayout != null) {
-            createDetailFragment(null);
-            mDrawerLayout.openDrawer(GravityCompat.END);
-        }
-        else {
-            Intent detail = new Intent(this, DetailActivity.class);
-            startActivity(detail);
-        }
+    private boolean isDetailFragmentShown() {
+        return drawerLayout != null && drawerLayout.isDrawerOpen(GravityCompat.END);
     }
 }
